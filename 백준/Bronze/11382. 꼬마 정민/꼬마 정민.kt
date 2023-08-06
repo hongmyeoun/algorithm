24a5b9c9f677ec3a1523691fb6c420ac63a2d990
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-08-06 20:19:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 13524 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+    val a = sc.nextLong()
+    val b = sc.nextLong()
+    val c = sc.nextLong()
+
+    
+    val result = a + b + c
+    println(result)
+}
