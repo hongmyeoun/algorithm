400c15c5ea4f6a9a4ad94ee874eb24334a19c9da
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-11 23:29:36 +0900

    refactor : f"", max 풀이 추가

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -2,3 +2,14 @@ def solution(a, b):
     a, b = str(a), str(b)
     answer = int(a + b) if int(a + b) > int(b + a) else int(b + a)
     return answer
+
+# 다른사람 풀이 1
+
+def solution(a, b):
+    return int(max(f"{a}{b}", f"{b}{a}"))
+
+# 다른사람 풀이 2
+
+def solution(a, b):
+    a, b = str(a), str(b)
+    return int(max(a+b, b+a))
