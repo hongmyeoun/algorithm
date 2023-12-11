0d2dc68d91b711ade257c61fa0dbbaa9ff90ea27
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-11 23:47:52 +0900

    refactor : max로 풀이한것 추가

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -3,3 +3,8 @@ def solution(a, b):
     a_str, b_str = str(a), str(b)
     answer = int(a_str + b_str) if int(a_str + b_str) > 2 * a * b else 2 * a * b
     return answer
+
+# 다른사람 풀이
+
+def solution(a, b):
+    return max(int(str(a) + str(b)), 2 * a * b)
