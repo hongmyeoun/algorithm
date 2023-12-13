036785003d0d7d151283f5f1c19609a4964ba2e6
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-13 22:46:32 +0900

    ref : 다른사람 풀이
    eval이라는 함수는 string들을 진짜 식처럼 계산해줌

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -13,3 +13,15 @@ def solution(ineq, eq, n, m):
     answer = 1 if ineq_bool else 0
     
     return answer
+
+# 다른사람 풀이
+def solution(ineq, eq, n, m):
+    # eval은 파이썬 표현식으로 평가하는 함수
+    # ex) answer = eval("2+5") // answer == 7 -> type == any
+    # 타입이 any이므로 int를 취해준다.
+    return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))
+
+def solution(ineq, eq, n, m):
+    if eq == '!':
+        eq = ''
+    return int(eval(f'{n} {ineq}{eq} {m}'))
