4cebf189e134133f8b8f4141f14e5f6ad0346c27
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-13 22:20:32 +0900

    ref : 다른사람의 풀이

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,3 +1,7 @@
 def solution(n):
     answer = sum([i for i in range(1, n+1, 2)]) if n % 2 == 1 else sum([i*i for i in range(2, n+1, 2)])
     return answer
+
+# 다른사람의 풀이
+def solution(n):
+    return sum(x ** (2 - x % 2) for x in range(n + 1) if n % 2 == x % 2)
