6a0b6a1fd3bd2e3c915d3834efdd44b6ffb54c07
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-13 22:10:38 +0900

    ref : 다른사람풀이

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,3 +1,7 @@
 def solution(number, n, m):
     answer = 1 if number % n == 0 and number % m == 0 else 0
     return answer
+
+# 다른사람 풀이
+def solution(number, n, m):
+    return int(bool(number % n == 0) & bool(number % m == 0))
