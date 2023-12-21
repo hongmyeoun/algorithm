343600353729ba4d27bb891f90e2bdeb0c52d2a0
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-21 23:15:31 +0900

    다른사람의 풀이 추가

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -19,3 +19,21 @@ def solution(code):
     answer = 'EMPTY' if ret == '' else ret
 
     return answer
+
+# 말도안되는 숏코딩
+def solution(code):
+    return "".join(code.split("1"))[::2] or "EMPTY"
+
+# 또다른 방법 ^=
+def solution(code):
+    answer = ''
+
+    mode = 0
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode ^= 1
+        else:
+            if i % 2 == mode:
+                answer += code[i]
+
+    return answer if answer else 'EMPTY'
