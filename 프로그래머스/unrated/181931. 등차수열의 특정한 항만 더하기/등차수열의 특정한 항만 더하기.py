0699327b1e81b1bb6446e3aa601e3d8777b3b16c
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-21 23:49:49 +0900

    ref : 다른사람코드

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -4,3 +4,14 @@ def solution(a, d, included):
         if i:
             sum_true += a + d * idx
     return sum_true
+
+# 다른사람 코드
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        answer += (a + d * i) * int(included[i])
+    return answer
+
+# 한줄코드
+def solution(a, d, included):
+    return sum(a + i * d for i, f in enumerate(included) if f)
