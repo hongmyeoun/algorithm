bc22bb3d5352425f935ac3eb3fe6da82d8d334d9
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 20:57:39 +0900

    ref: join 풀이 추가

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -8,3 +8,11 @@ def solution(num_list):
             even += str(i)
     
     return int(odd) + int(even)
+
+
+# join을 쓰는 방법 기억하자
+
+def solution(num_list):
+    even=int(''.join([str(i) for i in num_list if i%2==0]))
+    odd=int(''.join([str(i) for i in num_list if not i%2==0]))
+    return even+odd
