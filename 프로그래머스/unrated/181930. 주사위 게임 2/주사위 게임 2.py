e13099e76ece67bbfb4d8176e958b98eb4e4e77d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 20:25:00 +0900

    ref: 다른사람풀이

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -9,3 +9,13 @@ def solution(a, b, c):
         answer = a+b+c
         
     return answer
+
+# set을 이용한 풀이
+def solution(a, b, c):
+    check=len(set([a,b,c]))
+    if check==1:
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:
+        return (a+b+c)
