915394605b9d6bc6b5c0b27135a0665cec859765
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 23:01:28 +0900

    ref: queries를 두개로 받기

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -2,3 +2,9 @@ def solution(arr, queries):
     for query in queries:
         arr[query[0]], arr[query[1]] = arr[query[1]], arr[query[0]]
     return arr
+
+# 받을때부터 두개로 받기
+def solution(arr, queries):
+    for a,b in queries:
+        arr[a],arr[b]=arr[b],arr[a]
+    return arr
