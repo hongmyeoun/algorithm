ba89dd71883f51aba712db045fa48f72bb24d35d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 20:34:16 +0900

    ref: 다른사람풀이 추가

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -7,4 +7,11 @@ def solution(num_list):
         return 1
     else:
         return 0
-    
+
+# import math를 활용
+import math
+def solution(num_list):
+    answer = 0
+    if math.prod(num_list) < (sum(num_list)**2):
+        answer = 1
+    return answer
