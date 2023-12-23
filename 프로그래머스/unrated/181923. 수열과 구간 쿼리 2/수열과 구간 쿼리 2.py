c4380920abb46bf6d52f2328cd603c619fb3c102
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 23:46:46 +0900

    ref: temp를 사용한 다른사람 코드

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -3,16 +3,33 @@ def solution(arr, queries):
     answer = []
     for query in queries:
         s, e, k = query
-        # 처음 코드
-        # result = []
-        # for i in range(s, e + 1):
-        #     if arr[i] > k:
-        #         result = arr[i]
-        # if result:
-        #     answer.append(min(result))
-        # else:
-        #     answer.append(-1)
         result = [arr[i] for i in range(s, e + 1) if arr[i] > k]
         answer.append(min(result) if result else -1)
 
+    return answer
+
+# 처음 코드
+def solution(arr, queries):
+    answer = []
+    for query in queries:
+        s, e, k = query
+        result = []
+        for i in range(s, e + 1):
+            if arr[i] > k:
+                result = arr[i]
+        if result:
+            answer.append(min(result))
+        else:
+            answer.append(-1)
+    return answer
+    
+# 첫 코드와 비슷한 코드
+def solution(arr, queries):
+    answer = []
+    for s, e, k in queries:
+        tmp = []
+        for x in arr[s:e+1]:
+            if x > k:
+                tmp.append(x)
+        answer.append(-1 if not tmp else min(tmp))
     return answer
