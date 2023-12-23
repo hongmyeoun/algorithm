c609d7cc9476c644b24ae9ba0314d9db35e71182
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-23 21:37:12 +0900

    ref: dict, zip 활용

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -10,3 +10,8 @@ def solution(n, control):
         elif i == 'a':
             answer -= 10
     return answer
+
+# dictionary와 zip 생각을 왜못했지?
+def solution(n, control):
+    key = dict(zip(['w','s','d','a'], [1,-1,10,-10])) # zip([][]) => {'w': 1, 's': -1, 'd': 10, 'a': -10} 리스트를 튜프로 만들고, dict을 딕셔너리를 만듦
+    return n + sum([key[c] for c in control]) # 더해지고 빼지는 값의 총합과 n을 더하는 로직
