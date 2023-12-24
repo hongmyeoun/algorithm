218f6addd915b2cf3d5d2e3c1d2abe02e489302f
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-24 23:51:57 +0900

    ref: 쉽게 idx가져오기

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -3,3 +3,7 @@ def solution(my_string, index_list):
     answer = ''
     dic_str = dict(zip(list(range(0, len(my_string))), [c for c in my_string]))
     return ''.join([answer + dic_str[idx] for idx in index_list])
+
+# 너무 어렵게 생각했고, 그냥 str의 인덱스값을 불러오면됨...
+def solution(my_string, index_list):
+    return ''.join([my_string[idx] for idx in index_list])
