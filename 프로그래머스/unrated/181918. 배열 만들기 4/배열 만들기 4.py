cf0d202d1500beb6b29c9bf5494a38911a6df485
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-24 17:15:49 +0900

    ref: 다양한 풀이 추가

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -11,3 +11,25 @@ def solution(arr):
         elif stk[-1] >= arr[i]:
             stk.pop()
     return stk
+
+# stk가 없거나 마지막자리가 작으면 이조건만 다르고 실행은 같음
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        a = arr[i]
+        if len(stk)==0 or stk[-1] < a: 
+            stk.append(a)
+            i += 1
+        else:
+            stk.pop()
+    return stk
+
+# 반복문이 두개이긴함
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        while stk and stk[-1] >= arr[i]:
+            stk.pop()
+        stk.append(arr[i])
+    return stk
