55e61578291e1e0fc9bd896d071ead17a9559cf7
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-24 16:47:57 +0900

    ref: 차집합 이용

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -7,3 +7,17 @@ def solution(l, r):
     if not answer:
         answer = [-1]
     return answer
+
+# 차집합 이용
+def solution(l, r):
+    answer = []  # 결과를 저장할 리스트
+
+    # 숫자 l부터 r까지 반복
+    for num in range(l, r + 1):
+        # 숫자를 문자열로 변환하여 집합으로 만들고, {'0', '5'}와의 차집합을 확인
+        if not set(str(num)) - set(['0', '5']):
+            # 차집합이 비어 있다면 (즉, 모든 자릿수가 '0' 또는 '5'인 경우)
+            answer.append(num)  # 결과 리스트에 해당 숫자 추가
+
+    # 만약 결과 리스트가 비어 있다면 -1을 담은 리스트를 반환, 아니면 결과 리스트를 반환
+    return answer if answer else [-1]
