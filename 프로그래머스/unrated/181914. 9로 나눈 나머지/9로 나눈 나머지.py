83cb3a62964ad3dba46b5fc4192513f267b48c69
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-25 00:18:33 +0900

    ref: int로 풀기

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.py
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.py
@@ -1,2 +1,7 @@
 def solution(number):
     return eval('+'.join([digit for digit in number])) % 9
+
+# 다른사람 풀이라는데 문제와 맞는건가? 
+# 문제에서 요구하는건 각자리 수를 합했을때 그걸 9로 나눴을때 나머지가 그 수를 9로 나눈 나머지랑 같다는걸 알려줬으니 각자리 합으로 푸는건줄...
+def solution(number):
+    return int(number) % 9
