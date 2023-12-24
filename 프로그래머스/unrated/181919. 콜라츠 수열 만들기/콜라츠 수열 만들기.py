16d28f0d849a239579f9c7e1f96fa3f9ed81ab30
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-24 17:02:14 +0900

    ref: '//'를 사용해 몫구하기

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -4,3 +4,14 @@ def solution(n):
         n = n // 2 if not n % 2 else 3 * n + 1
         answer.append(n)
     return answer
+
+# 처음에 나는 /로 나눗셈을 했지만 //가 몫을 가져가는 것이므로 // 사용해야됨
+def solution(n):
+    answer = [n]
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        answer.append(n)
+    return answer
