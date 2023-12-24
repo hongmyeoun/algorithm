8435a22892b30ecaba5ed38f891fbee9fa193e03
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-25 00:48:11 +0900

    ref: [::1]로 reverse

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -3,3 +3,10 @@ def solution(my_string, queries):
     for s, e in  queries:
         my_string[s:e + 1] = list(reversed(my_string[s:e + 1]))
     return ''.join(my_string)
+
+# [::1] 을 통해 reverse
+def solution(my_string, queries):
+    answer=list(my_string)
+    for s,e in queries:
+        answer[s:e+1]=answer[s:e+1][::-1]
+    return ''.join(answer)
