4de2e9efd8268d4020b45f28fec2e69c8a27b178
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-24 16:20:11 +0900

    ref: not 코드 활용

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -6,3 +6,13 @@ def solution(arr, queries):
             if i % k == 0:
                 arr[i] = arr[i] + 1
     return arr
+
+# 다른 코드
+def solution(arr, queries):
+    answer = []
+    for s, e, k in queries:
+        for i in range(s, e+1):
+            # not은 나머지가 0이면 True 아니면 false
+            if not i % k:
+                arr[i] += 1
+    return arr
