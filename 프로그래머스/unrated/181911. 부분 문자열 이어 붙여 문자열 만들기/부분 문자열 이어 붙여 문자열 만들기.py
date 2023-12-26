85e705b04f0fdeb51e5e711e45d97406ee3d66d7
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-26 17:15:10 +0900

    ref: zip 풀이

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,2 +1,6 @@
 def solution(my_strings, parts):
     return ''.join([my_strings[i][parts[i][0]:parts[i][1] + 1] for i in range(0, len(my_strings))])
+
+# zip 풀이가 더 간단한거 같음 방법은 동일
+def solution(my_strings, parts):
+    return ''.join([x[y[0]:y[1]+1] for x,y in zip(my_strings, parts)])
