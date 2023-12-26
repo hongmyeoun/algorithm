4b39fba9f1f022ef57cb5be22bc68e287d69d9e0
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-26 16:47:44 +0900

    ref: lamda

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,2 +1,6 @@
 def solution(intStrs, k, s, l):
     return [int(i[s:s + l]) for i in intStrs if int(i[s:s+l]) > k]
+
+# 람다식으로 풀기
+def solution(intStrs, k, s, l):
+    return list(filter(lambda x: x > k, map(lambda x: int(x[s:s + l]), intStrs)))
