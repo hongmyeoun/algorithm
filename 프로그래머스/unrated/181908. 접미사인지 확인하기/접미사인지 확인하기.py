ed5afb8b88aac47ce1ccdec7a6a967d337341a84
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-27 22:22:09 +0900

    ref: str.endwith(suffix) 함수 상요

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,3 +1,15 @@
 def solution(my_string, is_suffix):
     my_string_suffix = [my_string[-i:] for i in range(len(my_string))]
     return 1 if is_suffix in my_string_suffix else 0
+
+# 다른 풀이 1
+is_suffix도 결국엔 접미사를 넣어주는거기 때문에 my_string에 뒷부분에 넣어주고 비교만 하면된다.
+def solution(my_string, is_suffix):
+    if my_string[-len(is_suffix):] == is_suffix: return 1
+    return 0
+
+# endwith()를 사용한 풀이
+str.endwith(suffix)는 문자열이 주어진 접미사로 끝나면 True, 아니면 False를 반환하는 함수
+int()는 True는 1 False는 0으로 반환하는 함수
+def solution(my_string, is_suffix):
+    return int(my_string.endswith(is_suffix))
