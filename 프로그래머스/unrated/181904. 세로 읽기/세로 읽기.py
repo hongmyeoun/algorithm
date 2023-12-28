862f325b2be1801df86674ba4756c0b3fec94bb4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 16:17:43 +0900

    ref : 충격의 슬라이싱

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,1 +1,14 @@
-solution = lambda my_string, m, c: my_string[c-1::m]
+def solution(my_string, m, c):
+    matrix = [list(my_string[i:i + m]) for i in range(0, len(my_string), m)] # range(start, end, step)
+    return ''.join([matrix[i][c-1] for i in range(len(matrix))])
+
+# 충격과 공포의 슬라이싱
+solution = lambda my_string, m, c: my_string[c-1::m] 
+# c-1 : 슬라이싱 시작 인덱스
+# ::m : 슬라이싱 간격
+# -> 이 풀이가 문제와 적합한지 의문이 듦, 2차원 리스트를 만들고 각 열에 대한 정보를 갖고오는 취지인것 같은 문제인데, 이 풀이는 str슬라이싱
+# 하지만 매우 합리적임
+
+# 이것도 비슷한 방법
+def solution(my_string, m, c):
+    return ''.join(my_string[i] for i in range(c - 1, len(my_string), m))
