3590f97afe3d99e393136739a88e536433ad2496
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 15:12:08 +0900

    ref : startewith

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,1 +1,7 @@
 solution = lambda my_string, is_prefix: int(my_string[:len(is_prefix)] == is_prefix)
+
+# startwith()도 있음... 왜 자동완성에 안나왔지?
+def solution(my_string, is_prefix):
+    return int(my_string.startswith(is_prefix))
+
+lambda my_string, is_prefix: int(my_string.startswith(is_prefix))
