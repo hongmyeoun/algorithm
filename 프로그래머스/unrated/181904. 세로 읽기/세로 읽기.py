21925ae99ec7a3cf5a9b2c507245a324595eb761
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 16:09:40 +0900

    [level unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,3 +1,1 @@
-def solution(my_string, m, c):
-    matrix = [list(my_string[i:i + m]) for i in range(0, len(my_string), m)]
-    return ''.join([matrix[i][c-1] for i in range(len(matrix))])
+solution = lambda my_string, m, c: my_string[c-1::m]
