94b7feaa6cedfde253a26c6ef3c61ed8f921b741
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 16:31:15 +0900

    ref : 문제를 이해하기

--- 프로그래머스/unrated/181903. qr code/qr code.py
+++ 프로그래머스/unrated/181903. qr code/qr code.py
@@ -1,1 +1,19 @@
 solution = lambda q, r, code: ''.join([code[i] if i % q == r else '' for i in range(len(code))])
+
+# 처음 풀이
+def solution(q, r, code):
+    answer = ''
+    for i in range(len(code)):
+        if i % q == r:
+            answer += code[i]
+    return answer
+
+# 두번째
+def solution(q, r, code):
+    return ''.join([code[i] if i % q == r else '' for i in range(len(code))])
+
+# 문제를 간단히 이해하기
+결국 나머지로 나오는 숫자는 반복되고 그 간격은 나눈 수와 같다.
+ex) 4로 나누면 나머지는 0, 1, 2, 3 -> 4개
+solution = lambda q, r, code): code[r::q] # r인덱스 부터 q간격으로 슬라이싱
+  
