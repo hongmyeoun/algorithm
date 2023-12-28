d6dd10c10740ebd89f678d9b0d9c5ac1bb99fac2
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 15:26:11 +0900

    ref : list로 풀이

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,1 +1,6 @@
 solution = lambda my_string, s, e: my_string[:s] + my_string[s:e + 1][::-1] + my_string[e + 1:]
+
+# list로 변환후 reversed -> 
+def solution(my_string, s, e):
+    substr = reversed(list(my_string[s:e+1])) # list(my_string[s:e+1])[::-1] 과 동일
+    return my_string[:s] + ''.join(substr) + my_string[e+1:]
