7ad90af2bf5e53109de0c01a179ee1232f7a2165
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 18:13:20 +0900

    ref : 리스트 컨프리헨션

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -11,3 +11,16 @@ def solution(n, k):
             break
         
     return answer
+
+# 위 코드 더 간결화
+def solution(n, k):
+    answer = []
+    i = 1
+    while (i * k) <= n: answer.append(i * k); i += 1
+    return answer
+
+# 리스트 컨프리헨션 답안
+def solution(n, k):
+    return [i for i in range(k,n+1,k)]
+
+k부터 시작해 더하는 형식
