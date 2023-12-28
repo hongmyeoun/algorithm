8b244868489ecfb793d871cf1166c464ab60d414
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-28 17:50:34 +0900

    ref : ord() 설명

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -4,3 +4,10 @@ def solution(my_string):
         idx = ord(c) - ord('A') if c.isupper() else ord(c) - ord('a') + 26
         answer[idx] += 1
     return answer
+
+ord() 는 해당 char의 아스키 코드값을 반환해줌
+ord(c) - ord('A')로 총 52의 길이를 갖는 answer에서 해당 문자의 위치를 결정
+
+# 다른사람 풀이
+def solution(my_string):
+    return [my_string.count(alphabet) for alphabet in 'abcdefghijklmnopqrstuvwxyz'.upper()+'abcdefghijklmnopqrstuvwxyz']
