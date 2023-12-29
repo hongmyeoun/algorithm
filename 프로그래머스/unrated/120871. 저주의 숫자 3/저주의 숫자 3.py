816fb2c222e2005ae341ac68c7bdbe98e00323c8
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-29 22:32:53 +0900

    ref : 필요한 만큼만 계산하는 방법

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,4 +1,13 @@
 def solution(n):
+    answer = 0
+    for _ in range(n):
+        answer += 1
+        while not answer%3 or '3' in str(answer):
+            answer += 1
+    return answer
+
+# 처음 풀이
+def solution(n):
     # 3x마을이 사용하는 숫자는 3의 배수와 숫자 3을 안씀
     # 3의 배수를 안씀
     # 3을 안씀
@@ -10,13 +19,17 @@ def solution(n):
     # n값을 저주마을 숫자리스트 index로 찾아냄
     # curse_numlist = []
     # # curse_numlist의 index가 n과 같아지면 종료
-    # for i in range(1000):
-    #     if not i%3 or '3' in str(i):
-    #         continue
-    #     curse_numlist.append(i)
-    answer = 0
-    for _ in range(n):
-        answer += 1
-        while not answer%3 or '3' in str(answer):
-            answer += 1
-    return answer
+    for i in range(1000):
+        if not i%3 or '3' in str(i):
+            continue
+        curse_numlist.append(i)
+    return curse_numlist[n-1]
+
+큰 수는 표현 불가, 리스트 형태 저장으로 메모리 낭비, 딱 필요한 만큼만 계산하는게 아니라서 속도 비효율
+
+이전에 range(100)으로 했는데, 이게 n의 값이 100까지지, curse_numlist의 길이가 100까지 안됐기 때문에 run time error가 떴던거 같음
+하지만 range(1000)으로 바꾸고 그냥 돌려봤는데 제출이 됨
+
+입력된 값만큼만 계산하도록 식을 수정하고 싶었음
+그러려면 n만큼 순회하고, while안에서 값이 증가하는 식을 만들어야 됐음
+그래서 range(n)만큼 순회를 하면서 값을 1씩 증가시키면서, 조건이 맞게되면 그 값을 스킵하는 방식으로 1을 더하는 방식을 채용
