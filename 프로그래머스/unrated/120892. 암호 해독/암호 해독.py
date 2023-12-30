8cc2c3f7a048ec1041e011be4f3f86563eabfb87
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2023-12-30 20:09:31 +0900

    ref : range 사용

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
@@ -1,3 +1,7 @@
 def solution(cipher, code):
     # 4만큼 뛰어넘어가면서 슬라이싱
     return cipher[code-1::code]
+
+# range로 슬라이싱
+def solution(cipher, code):
+    return ''.join(cipher[i] for i in range(code-1,len(cipher),code))
