faac85bddb36f10b96942cf55aefb1c109619c6b
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-02 17:23:29 +0900

    ref : 다양한 팩토리얼 구현

--- 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,14 +1,10 @@
+# 조합 라이브러리가 있지않을까?
 from math import comb
 
-# def solution(balls, share):
-#     return comb(balls, share)
+def solution(balls, share):
+    return comb(balls, share)
 
-# 의사코드
-    # 조합 라이브러리가 있지않을까?
-    
-    # 아니면 팩토리얼 함수 만들기
-    # n! = n(n-1)(n-2)...1
-    
+# 아니면 팩토리얼 함수 만들기
 def solution(balls, share):
     numer = factorial(balls)
     denom = factorial(balls - share) * factorial(share)
@@ -19,3 +15,34 @@ def factorial(n):
         return 1
     else: 
         return n * factorial(n-1)
+
+# 의사코드
+    # 조합 라이브러리가 있지않을까?
+    
+    # 아니면 팩토리얼 함수 만들기
+    # n! = n(n-1)(n-2)...1
+
+# 근데 팩토리얼 라이브러리도 있었음ㅋㅋ
+from math import factorial as f
+def solution(balls, share):
+    return f(balls)/(f(share)*f(balls-share))
+
+# 계산으로 풀이(팩토리얼을 한번에 계산)
+def solution(balls, share):
+    a, b = 1, 1
+    for i in range(1,share+1):
+        a *= balls
+        balls -= 1
+        b *= i
+    return int(a / b)
+
+# 다른방식의 팩토리얼
+def solution(balls, share):
+    answer = factorial(balls) / (factorial(balls - share) * factorial(share))
+    return answer
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
