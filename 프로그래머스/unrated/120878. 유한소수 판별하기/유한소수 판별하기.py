c4144855b8d73209bb28d4c5bce7cb57301c1950
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-02 17:01:07 +0900

    ref : 다양한 풀이방법

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -6,6 +6,7 @@ def solution(a, b):
     denom = b//gcd
     
     result = 0
+    
     if a == b:
         return 1
     
@@ -34,3 +35,33 @@ def prime_factors(n):
     # 최대공약수로 약분
     # 이후 분모의 소인수를 찾음
     # 소인수가 2 또는 5가 아니면 2를 return
+
+test case를 통과 못해서 몇개 추가
+1. a == b일때
+2. a가 b로 나누어 떨어질때
+이때도 유한소수이다.
+
+소인수에 2나 5가 아닌 다른수가 나왔을때 바로 break를 통해 결과를 return해야 테스트가 통과 됐다.
+
+# 다른 풀이
+from math import gcd
+def solution(a, b):
+    b //= gcd(a,b)
+    while b%2==0:
+        b//=2
+    while b%5==0:
+        b//=5
+    return 1 if b==1 else 2
+
+기약분수를 2로 나누고 5로나눠 몫이 1이 아니면 바로 무한소수인 방법
+
+# 비슷한 방법
+from math import gcd
+
+def solution(a, b):
+    b = b / gcd(a, b)
+    for i in [2, 5]:
+        while not b % i:
+            b //= i
+
+    return 1 if b == 1 else 2
