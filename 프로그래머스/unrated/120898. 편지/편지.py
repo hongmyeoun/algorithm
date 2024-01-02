29cb92b0c6dfa43a3a025441c73a7dc404ff2f42
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-02 20:44:41 +0900

    ref : 비트연산자 << 사용

--- 프로그래머스/unrated/120898. 편지/편지.py
+++ 프로그래머스/unrated/120898. 편지/편지.py
@@ -1,1 +1,10 @@
 solution = lambda message: len(message)*2
+
+# 비트연산자
+def solution(message):
+    return len(message)<<1
+  
+<<: 비트 왼쪽 시프트 연산자
+왼쪽으로 1만큼 비트를 이동
+
+len(message) << 1은 문자열 message의 길이를 2배한 값을 나타냄
