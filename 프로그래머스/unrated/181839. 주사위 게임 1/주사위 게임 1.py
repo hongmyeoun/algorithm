f7dbda53fded7816d3f7f57c6992751a75e0693e
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 03:05:16 +0900

    ref : 조건문 활용법

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -4,3 +4,14 @@ def solution(a, b):
     elif (a%2 == 0 and b%2 == 1) or (a%2 == 1 and b%2 == 0):
         return 2 * (a + b)
     return abs(a-b)
+
+# if문 조건 간소화
+def solution(a, b):
+    if a%2 and b%2: 
+        return a*a+b*b
+    elif a%2 or b%2: # 둘 중 하나만 참이라도 true니깐 둘쭝 하나만 홀수여도 된다는 뜻
+        return 2*(a+b)
+    return abs(a-b)
+
+0은 false
+1은 true
