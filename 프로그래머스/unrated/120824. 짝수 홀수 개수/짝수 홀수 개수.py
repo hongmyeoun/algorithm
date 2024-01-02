9d5915e0031961b111a1c62ae8cfcec971bf2e0a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 01:07:09 +0900

    ref : 효과적인 풀이

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,10 @@
 def solution(num_list):
     answer = [1 if i % 2 == 0 else 2 for i in num_list]
     return [answer.count(1), answer.count(2)]
+
+# 다른풀이
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
