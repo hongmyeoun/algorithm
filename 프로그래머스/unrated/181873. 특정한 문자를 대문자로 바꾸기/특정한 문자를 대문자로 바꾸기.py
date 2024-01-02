141a3225f3e153c7e6a5f9ff335eedbab008c485
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 02:53:49 +0900

    ref : 문제를 잘 읽어보고 좀 생각하고 풀기

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -3,3 +3,7 @@ def solution(my_string, alp):
         if c == alp:
             my_string = my_string.replace(c, c.upper())
     return my_string
+
+# 그냥 이렇게 하면된다...
+def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
