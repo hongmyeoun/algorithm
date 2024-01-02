bb5da76bd3ae4e2060be5323a798e0355ce16c5d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 02:09:43 +0900

    ref : isdigit()

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,4 +1,7 @@
 def solution(my_string):
     num = ['1','2','3','4','5','6','7','8','9']
-            
     return sum(int(c) for c in my_string if c in num)
+
+# isdisigt() 숫자인지 판별해주는거 같음
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
