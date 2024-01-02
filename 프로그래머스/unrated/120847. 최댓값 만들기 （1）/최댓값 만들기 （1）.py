9012bcf548260ea8c49822aaa01ce4526f63f6d4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 01:23:13 +0900

    ref : [-1]

--- 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,3 +1,8 @@
 def solution(numbers):
     numbers.sort()
     return numbers[len(numbers)-1] * numbers[len(numbers)-2]
+
+# 인덱싱 연습좀...
+def solution(numbers):
+    numbers.sort()
+    return numbers[-2] * numbers[-1]
