5fe7a0535d26061e2c294cbeb61f0eea75a3aedb
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 04:07:17 +0900

    ref : 다른 직관적인 풀이

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -7,3 +7,16 @@ def solution(arr, n):
             if idx%2:
                 arr[idx] = i+n
     return arr
+
+# 직관적인 코드(for문을 조건에 따라서 돌리는데 이게 더 효율적인가?)
+def solution(arr, n):
+    if len(arr) % 2:
+        for i in range(0, len(arr), 2):
+            arr[i] += n
+    else:
+        for i in range(1, len(arr), 2):
+            arr[i] += n
+
+    return arr
+
+효율차이는 없다고 한다. 다만 위 코드에 불필요한 enumerate()함수를 없에면 더 직관적일 수 있다고 한다.
