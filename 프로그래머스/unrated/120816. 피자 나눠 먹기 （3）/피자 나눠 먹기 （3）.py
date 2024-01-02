4c49f3209d719deb992b181054035eb8524b1ff4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-02 20:41:39 +0900

    ref : divmod 풀이 추가

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,11 +1,18 @@
-# def solution(slice, n):
-#     if n > slice:
-#         if n % slice == 0:
-#             return n//slice
-#         else:
-#             return n//slice + 1
-#     else:
-#         return 1
+def solution(slice, n):
+    if n > slice:
+        if n % slice == 0:
+            return n//slice
+        else:
+            return n//slice + 1
+    else:
+        return 1
 
 def solution(slice, n):
     return (n-1)//slice + 1
+
+# divmod 사용
+def solution(slice, n):
+    d, m = divmod(n, slice)
+    return d + int(m != 0)
+
+int(m != 0) : int(true) -> 1반환 아니면 0반환
