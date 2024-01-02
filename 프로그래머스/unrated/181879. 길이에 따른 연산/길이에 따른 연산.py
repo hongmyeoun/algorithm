a8e290abefb416859dd448f8ed13508069a79370
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 03:16:51 +0900

    ref : math 라이브러리 prod

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -7,3 +7,8 @@ def solution(num_list):
         result *= i
         
     return result
+
+# 라이브러리 사용 prod
+from math import prod
+def solution(num_list):
+    return sum(num_list) if len(num_list)>=11 else prod(num_list)
