c880800f64e98e9751d50cbc32efeb295dc6c3b2
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 01:33:50 +0900

    ref : sort의 파라미터

--- 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -3,3 +3,8 @@ def solution(array, height):
     array.sort()
     array = array[::-1]
     return array.index(height)
+
+def solution(array, height):
+    array.append(height)
+    array.sort(reverse=True) # reverse 파라미터가 있음
+    return array.index(height)
