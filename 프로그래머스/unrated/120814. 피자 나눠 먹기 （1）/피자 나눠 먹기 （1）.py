c8dd1d03b0fe27e4f046a9438dc15a52d3d54861
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-02 20:16:15 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -0,0 +1,14 @@
+def solution(n):
+    if n > 7:
+        if n % 7 == 0:
+            return n // 7
+        else:
+            return n // 7 + 1
+    else:
+        return 1
+
+# 의사코드
+    # 피자 한판 개수 = 7 * x
+    # n 7 피자 7, 사람 7 -> 1
+    # n 1 피자 7, 사람 1 -> 1
+    # n 15 사람 15, 피자 21 -> 3
