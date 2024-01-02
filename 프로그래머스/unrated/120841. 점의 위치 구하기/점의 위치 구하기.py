7e6d7c2f99973c693e094912aaeda294eb6fad63
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 01:38:34 +0900

    ref : 조건문 결과(0, 1) 을 이용한 풀이

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -9,3 +9,9 @@ def solution(dot):
             return 2
         else:
             return 3
+
+def solution(dot):
+    quad = [(3,2),(4,1)]
+    return quad[dot[0] > 0][dot[1] > 0]
+
+좀 더 발전시켜서 quad = [(2, 1), (3, 4)]; return quad[dot[1]<0][dot[0]>0] 로 하면, 사분면 위치랑도 맞아서 좋네요
