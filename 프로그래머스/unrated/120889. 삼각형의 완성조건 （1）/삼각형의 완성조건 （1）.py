a2b3573cd469780415c51818f5be60d4bc346f95
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 02:20:11 +0900

    ref : 로직이 다른 수학식

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,3 +1,7 @@
 def solution(sides):
     sides.sort()
     return 1 if sides[0] + sides[1] > sides[-1] else 2
+
+# 총합 - 최대 = 나머지
+def solution(sides):
+    return 1 if max(sides) < (sum(sides) - max(sides)) else 2
