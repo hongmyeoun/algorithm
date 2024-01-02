2b273f10ac0e4e0e965980383fdfa23d9094d293
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 03:40:17 +0900

    ref : swapcase()활용

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,2 +1,6 @@
 def solution(my_string):
     return ''.join([c.lower() if c.isupper() else c.upper() for c in my_string])
+
+# swapcase() 대소문자를 바꿔줌
+def solution(my_string):
+    return my_string.swapcase()
