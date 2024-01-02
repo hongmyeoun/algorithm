a2ccf83d3dd53e31bf4c9eef452f0035d6b8fe5f
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 01:29:51 +0900

    ref : 한번에 확인하기

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
@@ -2,3 +2,7 @@ def solution(my_string):
     for c in ['a','e','i','o','u']:
         my_string = my_string.replace(c, '')
     return my_string
+
+# 숏코딩 힙스터
+def solution(my_string):
+    return "".join([i for i in my_string if not(i in "aeiou")])
