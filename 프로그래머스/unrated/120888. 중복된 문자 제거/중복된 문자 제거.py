dfccc35a3c98e995aabb42aaa654a567621df5e0
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 22:58:06 +0900

    ref : dict.formkeys() 활용

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -4,3 +4,9 @@ def solution(my_string):
         if c not in result:
             result.append(c)
     return ''.join(result)
+
+# 키로 만들어서 중복 제거
+def solution(my_string):
+    return ''.join(dict.fromkeys(my_string))
+
+제거후 다시 키값을 붙임
