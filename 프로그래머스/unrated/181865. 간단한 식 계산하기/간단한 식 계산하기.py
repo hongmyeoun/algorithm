c25a82c89bc113c498d28fc7a2a0fceb38d88c83
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 19:49:12 +0900

    ref : eval()을 안쓴 풀이

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,2 +1,18 @@
 def solution(binomial):
     return eval(binomial)
+
+# 그냥 풀이
+def solution(binomial):
+    a, op, b = binomial.split()
+
+    a = int(a)
+    b = int(b)
+
+    if op == "+":
+        result = a + b
+    elif op == "-":
+        result = a - b
+    elif op == "*":
+        result = a * b
+
+    return result
