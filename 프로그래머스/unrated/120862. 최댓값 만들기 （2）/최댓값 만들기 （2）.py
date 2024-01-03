5f5f10bc0c9da4f1c405972beb7eaf35f3c53b93
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 18:13:27 +0900

    ref : max() 사용

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,3 +1,8 @@
 def solution(numbers):
     numbers.sort()
     return numbers[-1] * numbers[-2] if numbers[-1] * numbers[-2] > numbers[0] * numbers[1] else numbers[0] * numbers[1]
+
+# max() 사용
+def solution(numbers):
+    numbers = sorted(numbers)
+    return max(numbers[0] * numbers[1], numbers[-1]*numbers[-2]) 
