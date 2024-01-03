b082bdcfceb1b5f5e4e7fee3cf1d16241791c54b
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 18:42:22 +0900

    ref : isdigit

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,3 +1,7 @@
 def solution(my_string):
     num = ['0','1','2','3','4','5','6','7','8','9']
     return sorted([int(c) for c in my_string if c in num])
+
+# isdisgit 또 기억하기
+def solution(my_string):
+    return sorted([int(c) for c in my_string if c.isdigit()])
