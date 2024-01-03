b29d70c36ac7180a3c37832d1fbaecf8d09e21d5
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 21:37:08 +0900

    ref : pat을 바꿔서 푸는 풀이

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -9,3 +9,7 @@ def solution(myString, pat):
     myString = ''.join(myString)
     
     return 1 if pat in myString else 0
+
+# pat을 바꾸는 풀이
+def solution(myString, pat):
+    return int(''.join(['A' if i == 'B' else 'B' for i in pat]) in myString)
