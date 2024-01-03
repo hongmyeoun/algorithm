e92f0cb11dba18993bedcdab957fd73a1e56ae1e
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 17:36:03 +0900

    ref : remove() 활용

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -4,3 +4,15 @@ def solution(arr, delete_list):
         if not (arr[i] in delete_list):
             result.append(arr[i])
     return result
+
+# 간단하게 
+def solution(arr, delete_list):
+    return [i for i in arr if i not in delete_list]
+
+# remove 활용
+def solution(arr, delete_list):
+    answer = []
+    for i in delete_list:
+        if i in arr:
+            arr.remove(i)
+    return arr
