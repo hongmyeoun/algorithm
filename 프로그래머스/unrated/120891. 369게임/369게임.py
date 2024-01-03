14c594a4a27e5595f52b1adc7caef206191a79ef
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 22:29:02 +0900

    ref : count로 369

--- 프로그래머스/unrated/120891. 369게임/369게임.py
+++ 프로그래머스/unrated/120891. 369게임/369게임.py
@@ -5,3 +5,8 @@ def solution(order):
         if i == '3' or i == '6' or i == '9':
             count += 1
     return count
+
+# count 사용
+def solution(order):
+    order = str(order)
+    return order.count('3') + order.count('6') + order.count('9')
