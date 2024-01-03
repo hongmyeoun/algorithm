992a426c093a5fb80346cd6dc06b002411c6b97b
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 21:55:33 +0900

    ref : split을 두번 사용해서 공백 없애기

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,2 +1,7 @@
 def solution(myString):
     return sorted([i for i in myString.split('x') if i != ''])
+
+# split을 두번사용
+def solution(myString):
+    answer = ' '.join(myString.split('x')).split()
+    return sorted(answer)
