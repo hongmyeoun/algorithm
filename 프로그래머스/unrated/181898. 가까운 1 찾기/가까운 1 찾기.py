74c0a1f285e49fbba07cff4f4306c4ccd525bf09
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-03 18:09:59 +0900

    range(start, end)

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -3,3 +3,10 @@ def solution(arr, idx):
         if idx <= index and i:
             return index
     return -1
+
+# range로 해결
+def solution(arr, idx):
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
+    return -1
