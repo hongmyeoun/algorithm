8c7477730f7ad817a4c07077953b130d79370384
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-04 23:20:52 +0900

    ref : 다른풀이

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -8,3 +8,14 @@ def solution(arr, flag):
             for _ in range(i):
                 X.pop()    
     return X
+
+# enumerate()로 풀이
+def solution(arr, flag):
+    X = []
+    for i, f in enumerate(flag):
+        if f:
+            X += [arr[i]] * (arr[i]*2)
+        else:
+            for _ in range(arr[i]):
+                X.pop()
+    return X
