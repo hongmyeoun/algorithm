9fb69efb0610736ec85917ef6a2e8161f0c7ef64
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-04 23:57:15 +0900

    ref : bin()로 계산

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -10,3 +10,11 @@ def solution(num_list):
                 temp //= 2
                 answer += 1
     return answer
+
+# bin로 풀이
+def solution(num_list):
+    return sum(len(bin(i)) - 3 for i in num_list)
+
+len(bin(i)) - 3을 통해 '0b'의 길이 2와 마지막 비트(1)의 길이 1을 뺍니다. 
+이는 정수 i를 1로 만드는데 필요한 연산 횟수와 일치합니다. 
+예를 들어, 10은 이진수로 1010이고, 3번의 연산이 필요합니다 (첫 번째 '1'은 무시됩니다).
