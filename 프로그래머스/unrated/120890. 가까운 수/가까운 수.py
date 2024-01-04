05b054f68c11fe98ef8fe36ce4aa86e6bb85712a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 01:10:16 +0900

    ref : 생각을 달리해보기

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.py
@@ -7,3 +7,14 @@ def solution(array, n):
             close = abs(i-n)
             result = i
     return result
+
+
+# 간단하게
+solution=lambda a,n:sorted(a,key=lambda x:(abs(x-n),x))[0]
+
+def solution(array, n):
+    array.sort(key = lambda x : (abs(x-n), x-n))
+    answer = array[0]
+    return answer
+
+그냥 정렬을 n과의 거리순으로 하면 되는거였음 ㅠㅠ
