5a9d4e760ff4bff5055640c1c0e69cd5fe2743e6
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 02:55:36 +0900

    ref : 라떼로 판별

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -8,3 +8,13 @@ def solution(order):
         elif menu in cafelatte:
             answer += 5000
     return answer
+
+# 라떼로 판별하기
+def solution(order):
+    answer = 0
+    for want in order:
+        if 'latte' in want:
+            answer += 500
+        answer += 4500
+
+    return answer
