1a2186811a18767e0dcf2a5087d54cf44208e9b0
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 00:22:49 +0900

    ref : sorted(str)

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.py
@@ -3,3 +3,12 @@ def solution(before, after):
     after = sorted([c for c in after])
     
     return 1 if before == after else 0
+
+# str을 sorted를 하면 굳이 list를 만들 필요가 없음
+def solution(before, after):
+    before=sorted(before)
+    after=sorted(after)
+    if before==after:
+        return 1
+    else:
+        return 0
