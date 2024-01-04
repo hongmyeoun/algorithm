5a444d8b4de63a60b021c00c05484b2be08b1f0a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 01:45:45 +0900

    ref : for 반복 줄이기

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -4,3 +4,10 @@ def solution(s):
         if s.count(c) == 1:
             answer += c
     return ''.join(sorted(answer))
+
+
+# 같은 식
+def solution(s):
+    return "".join(sorted([ch for ch in s if s.count(ch) == 1]))
+
+효율적으로 하려면 for문 횟수를 줄이기 위해 for c in s를 for c in set(s)로 바꿔준다
