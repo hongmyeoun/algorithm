d17cfb4cc6fd2466680c8eec5cdcb3514225d4db
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-04 22:52:27 +0900

    ref : 리스트컴프리헨션답

--- 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -5,3 +5,9 @@ def solution(board, k):
             if i + j <= k:
                 answer += board[i][j]
     return answer
+
+# 두줄 코드
+def solution(board, k):
+    return sum(board[i][j] for i in range(len(board)) for j in range(len(board[0])) if i + j <= k)
+
+리스트 컴프리헨션으로 더하면됨, 왜 나는 테스트할때 result = []로 만들어놓고 sum할 생각을 안했을까?
