d10df5d398f9e9ed9ccd7f56c664690507539b49
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 02:15:41 +0900

    ref : 쉽게 풀기

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -7,4 +7,13 @@ def solution(arr):
         return [-1]
     except IndexError:
         return [-1]
-    
+
+# 효율적인 풀이
+def solution(arr):
+    if 2 not in arr:
+        return [-1]
+    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
+
+2가 없다면 [-1] 반환
+아니면
+arr.index(2)부터 arr의 길이 - 2가있는 마지막 인덱스
