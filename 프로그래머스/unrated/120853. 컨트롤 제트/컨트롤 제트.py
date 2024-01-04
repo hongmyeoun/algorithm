1b8c6ce7174249ef92f8726654e6f78f6e575bc2
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 02:49:25 +0900

    ref : pop()을 이용한 풀이

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,3 +1,15 @@
 def solution(s):
     s = s.split()
     return sum(-int(s[i-1]) if s[i] == 'Z' else int(s[i]) for i in range(len(s)))
+
+# pop()을 이용한 풀이
+def solution(s):
+    stack = []
+    for a in s.split():
+        if a != 'Z':
+            stack.append(int(a))
+        else:
+            if stack:
+                stack.pop()
+
+    return sum(stack)
