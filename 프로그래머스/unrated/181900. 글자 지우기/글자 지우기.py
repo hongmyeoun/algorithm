0203d379d05912579985c04963a16b95961e1b0a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-04 23:13:06 +0900

    ref : not을 사용

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -3,3 +3,10 @@ def solution(my_string, indices):
     for i in indices:
         my_string[i] = ''
     return ''.join(my_string)
+
+# not 사용
+def solution(my_string, indices):
+    answer = ''
+    for i in range(len(my_string)):
+        if i not in indices:answer+=my_string[i]
+    return answer
