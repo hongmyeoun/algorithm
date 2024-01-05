f9dcd15394708fdff693963cd36d7ae076f03fa6
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 13:45:46 +0900

    ref : 쉽게 풀이

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -5,3 +5,14 @@ def solution(arr, k):
     if len(arr_set[:k]) < k:
         return arr_set[:k] + [-1]*(k-len(arr_set[:k]))
     return arr_set[:k]
+
+# not in으로 유니크 찾기
+def solution(arr, k):
+    ret = []
+    for i in arr:
+        if i not in ret:
+            ret.append(i)
+        if len(ret) == k:
+            break
+
+    return ret + [-1] * (k - len(ret))
