f9188a61db44c8462ea8834afba9b0810fe4412f
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-05 15:30:40 +0900

    ref : max, min으로 최적값 확인

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -19,3 +19,18 @@ def solution(keyinput, board):
             if answer[1] != end_line_vu:
                 answer[1] += 1
     return answer
+
+# max, min으로 최적값으로 가기
+def solution(keyinput, board):
+    curr = [0, 0]
+    for k in keyinput:
+        if k == 'left':
+            curr[0] = max(curr[0] - 1, -(board[0] // 2))
+        elif k == 'right':
+            curr[0] = min(curr[0] + 1, board[0] // 2)
+        elif k == 'down':
+            curr[1] = max(curr[1] - 1, -(board[1] // 2))
+        else:
+            curr[1] = min(curr[1] + 1, board[1] // 2)
+
+    return curr
