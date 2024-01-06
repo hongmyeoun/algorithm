c5dbef8653ca22b43a6533a70aa781030bad426d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-06 16:34:08 +0900

    ref : split으로 ''만 없앨수 있음

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
@@ -6,3 +6,8 @@ def solution(myStr):
     answer = [c for c in myStr.split('1') if c != '']
 
     return answer if answer else ['EMPTY']
+
+# split으로 공백없애기
+def solution(myStr):
+    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+    return answer if answer else ['EMPTY']
