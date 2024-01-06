a7a41c02b484e4eda8c3d4d0089d83b6174f0258
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-06 17:00:21 +0900

    ref : 거듭제곱 확인법

--- 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
+++ 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -4,3 +4,19 @@ def solution(arr):
         zeros = abs(2**length.bit_length() - length)
         arr += [0] * zeros
     return arr
+
+# 다른 풀이
+def solution(arr):
+    a = 1
+    b = len(arr)
+    while a < b :
+        a *= 2
+    return arr + [0] * (a-b)
+
+
+"""
+2의 거듭제곱 확인 방법 O(1)로 해야 겨우 O(n)
+logn으로 확인하면 그걸 n번 반복 -> o(nlogn)
+길이 1000개 -> 가장 빠른방법은 각각 하드코딩하기 겨우 10개
+
+"""
