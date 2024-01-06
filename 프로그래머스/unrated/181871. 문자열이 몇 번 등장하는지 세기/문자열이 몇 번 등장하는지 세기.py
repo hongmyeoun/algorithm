ea17c7a722d98c643eba8518abd2cfdb413fe664
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-06 16:46:56 +0900

    ref : startswith

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -12,3 +12,12 @@ def solution(myString, pat):
         count += 1
         
     return count
+
+# startswith(pat): 현재 부분 문자열이 주어진 패턴 pat으로 시작하는지 여부를 확인합니다.
+
+def solution(myString, pat):
+    answer = 0
+    for i, x in enumerate(myString) :
+        if myString[i:].startswith(pat) :
+            answer += 1
+    return answer
