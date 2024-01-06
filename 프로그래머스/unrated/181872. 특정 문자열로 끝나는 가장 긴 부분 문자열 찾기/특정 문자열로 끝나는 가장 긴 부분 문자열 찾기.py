0e8c6689f3ec67841c96cd71e3349169f789f0b0
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-06 16:41:44 +0900

    ref : rfind, rindex

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -1,3 +1,6 @@
 def solution(myString, pat):
     idx = myString.rfind(pat)
     return myString[:idx + len(pat)] if idx != -1 else myString
+
+# rfind와 rindex
+solution=lambda x,y:x[:x.rindex(y)+len(y)]
