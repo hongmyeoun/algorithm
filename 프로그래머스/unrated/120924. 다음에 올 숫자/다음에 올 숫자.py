934a4d55dc6cbb826d9f2b644fc56694d8dba2c3
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-07 20:55:04 +0900

    ref : 문제에서 요구하는 것만 확인

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -11,3 +11,13 @@ def is_arithmetic(arr):
 def is_geometric(arr):
     ratios = [arr[i+1]/arr[i] for i in range(len(arr)-1)]
     return all(ratio == ratios[0] for ratio in ratios)
+
+# 함수 안나누고 간단하게 하는 방법
+def solution(common):
+    answer = 0
+    a,b,c = common[:3]
+    if (b-a) == (c-b):
+        return common[-1]+(b-a)
+    else:
+        return common[-1] * (b//a)
+    return answer
