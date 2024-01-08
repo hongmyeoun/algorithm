3bcd226ba955e2204d84125261a195e052f4df70
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-08 15:58:51 +0900

    ref : update로 풀이 추후에 다시 풀어볼것

--- 프로그래머스/unrated/120866. 안전지대/안전지대.py
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.py
@@ -9,3 +9,14 @@ def solution(board):
                         if -1 < i + x < n and -1 < j + y < n: # 범위지정 0<= <=길이-1
                             coordinate.add((i + x, j + y)) # 1인 좌표 등록(중복 제외)
     return n * n - len(coordinate)
+
+# update로 풀이
+def solution(board):
+    n = len(board)
+    danger = set()
+    for i, row in enumerate(board):
+        for j, x in enumerate(row):
+            if not x:
+                continue
+            danger.update((i+di, j+dj) for di in [-1,0,1] for dj in [-1, 0, 1])
+    return n*n - sum(0 <= i < n and 0 <= j < n for i, j in danger)
