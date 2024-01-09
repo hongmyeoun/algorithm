3a05887be7ecf716196461599149a0325c8ba3ad
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-09 23:26:56 +0900

    ref : 약수 효율

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,2 +1,10 @@
 def solution(n):
     return sum(i for i in range(1,n+1) if n%i==0)
+
+# 0.5제곱만 판단
+def sumDivisor(num):
+    return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0])
+
+12 -> 1, 2, 3, 4, 6, 12
+약수는 자신의 반을 넘어가는 수보다 클수 없음
+반을 넘는 수는 자기 자신만 있기 때문 num을 앞에 더해줌
