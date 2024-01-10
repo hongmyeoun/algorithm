6bbbf3ab5f7ad25dd91c3db0b0c1ae93f4eef390
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-10 22:33:12 +0900

    ref : list(str(n))

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -2,3 +2,11 @@ def solution(n):
     num_list = [int(i) for i in str(n)]
     num_list.sort(reverse=True)
     return int(''.join([str(i) for i in num_list]))
+
+# 같은 식 간단하게
+def solution(n):
+    ls = list(str(n))
+    ls.sort(reverse = True)
+    return int("".join(ls))
+
+정수를 list하면 나눠져서 들어간다
