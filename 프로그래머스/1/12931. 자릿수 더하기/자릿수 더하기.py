e48a7d0a199f9cb56a3c363f5428afe2cd8867e6
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-10 22:19:20 +0900

    ref : 재귀구조

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,3 +1,11 @@
 def solution(n):
     
     return sum(int(i) for i in str(n))
+
+# 재귀 구조
+def sum_digit(number):
+    '''number의 각 자릿수를 더해서 return하세요'''
+    if number < 10:
+        return number
+
+    return number%10 + sum_digit(number//10)
