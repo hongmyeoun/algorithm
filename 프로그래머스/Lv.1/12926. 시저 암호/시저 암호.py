da8b95ba9288f1eec2a97b05d925a2a938027b16
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-12 01:53:51 +0900

    ref : 알파벳 부등호

--- 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.py
@@ -11,3 +11,15 @@ def solution(s, n):
         else:
             answer += alphabet[alphabet.index(c) + n]
     return answer
+
+# 부등호로 확인하기
+def solution(s, n):
+    answer = ''
+    for i in s:
+        if i:
+            if i >= 'A' and i <= 'Z':
+                answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+            elif i >= 'a' and i <= 'z':
+                answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
+            else : answer += ' '
+    return answer
