62385ffa3f678b0cc761d3614c604d5bcc63747c
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-11 19:44:28 +0900

    ref : 등차수열의합

--- 프로그래머스/Lv.1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/Lv.1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -4,3 +4,7 @@ def solution(price, money, count):
         pay += price * i
 
     return pay-money if pay-money > 0 else 0
+
+# 등차수열
+def solution(price, money, count):
+    return max(0,price*(count+1)*count//2-money)
