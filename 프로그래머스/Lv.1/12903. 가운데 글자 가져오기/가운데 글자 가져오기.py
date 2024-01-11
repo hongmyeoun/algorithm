feecb8da82e8f311b44a5f9f892bce0a8e5e93c4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-11 18:20:59 +0900

    ref : 슬라이싱

--- 프로그래머스/Lv.1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/Lv.1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,2 +1,12 @@
 def solution(s):
     return s[len(s)//2] if len(s)%2 else s[len(s)//2-1:len(s)//2+1]
+
+# else를 쓸 필요가 없음
+def string_middle(str):
+    return str[(len(str)-1)//2 : len(str)//2 + 1]
+
+홀수면 
+5 -> 2:3 -> 2
+
+짝수면
+4 -> 1:3 -> 1,2
