15544f340b238f4a044b97c3dbf8948977cdb842
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-11 20:02:10 +0900

    ref : zip으로 더 간단히 가능

--- 프로그래머스/Lv.1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/Lv.1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,2 +1,7 @@
 def solution(arr1, arr2):
     return [[arr1[i][j]+arr2[i][j] for j in range(len(arr1[i]))] for i in range(len(arr1))]
+
+
+# zip으로 더 간단히
+def sumMatrix(A,B):
+    return [[c + d for c, d in zip(a,b)] for a, b in zip(A,B)]
