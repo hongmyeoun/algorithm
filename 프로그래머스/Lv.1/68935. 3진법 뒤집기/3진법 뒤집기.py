7b6a07f606f33d98320207af745bb303a884209b
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-11 20:38:12 +0900

    ref : 더 간단한 풀이

--- 프로그래머스/Lv.1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/Lv.1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -11,3 +11,16 @@ def tri(n):
         result = str(remainder) + result
     
     return result
+
+# 간단하게
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    answer = int(tmp, 3)
+    return answer
+
+위 풀이에서 str(r) + result를 뒤집은 방식 이렇게
+위는 정석으로 3진수를 나타내고 싶었음
