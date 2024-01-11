bdcb1ea977b9da053ee1198fe3098e8c394ff7f2
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-11 20:08:35 +0900

    ref : 라이브러리 사용

--- 프로그래머스/Lv.1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/Lv.1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,5 +1,10 @@
 import math
 
 def solution(n, m):
-    answer = [math.gcd(n,m), n*m//math.gcd(n,m)]
-    return answer
+    return [math.gcd(n,m), n*m//math.gcd(n,m)]
+
+# 직접 풀이
+def solution(n, m):
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
