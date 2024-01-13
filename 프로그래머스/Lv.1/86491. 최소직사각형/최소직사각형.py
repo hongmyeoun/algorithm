66a1e3853b77393b75a543498961a8721468064d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-13 23:43:24 +0900

    [level Lv.1] Title: 최소직사각형, Time: 4.08 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,34 @@
+def solution(sizes):
+    answer = 0
+    left = []
+    right = []
+    for i in sizes:
+        left.append(max(i))
+        right.append(min(i))
+        
+    return max(left) * max(right)
+
+# 14 4
+# 19 6
+# 16 6
+# 18 7
+# 11 7
+
+# 19 7
+
+# 10 7
+# 12 3
+# 15 8
+# 14 7
+# 15 5
+
+# 15 8
+
+# 60 50
+# 70 30
+# 60 30
+# 80 40
+
+# 80 50
+
+# [max, min]으로 정렬한뒤, max의 max값과 min의 max값을 곱함
