3e97136572283849ada6dc99bde727582ac6097b
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-14 01:39:14 +0900

    ref : del list [-1]

--- 프로그래머스/Lv.1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/Lv.1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -14,3 +14,14 @@ def solution(k, score):
 # k크기의 list에 score를 추가
 # 하지만 k의 가장 낮은 점수보다 낮으면 추가 X
 # 매 회 있는 k에 min값 append
+
+def solution(k, score):
+    answer = []
+    a=[]
+    for i in score:
+        a.append(i)
+        a.sort(reverse=True)
+        if len(a)>k:
+            del a [-1] # 리스트 a의 마지막 요소를 제거
+        answer.append(min(a))
+    return answer
