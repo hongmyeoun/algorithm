34a638eb09532938de607093daf3a777022162de
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-14 00:05:04 +0900

    ref : 쓸데없는 조건문 줄이기

--- 프로그래머스/Lv.1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/Lv.1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -12,3 +12,20 @@ def solution(food):
 
 # 각 음식이 홀수면 -1만큼 반을 나눠서 숫자를 넣음
 # 3이 다 들어갔으면
+
+# 양옆으로 중앙에서부터 채우는 방식
+def solution(food):
+    answer ="0"
+    for i in range(len(food)-1, 0,-1):
+        c = int(food[i]/2)
+        while c>0:
+            answer = str(i) + answer + str(i)
+            c -= 1
+    return answer
+
+# 나랑 똑같지마 조건문이 줄어드는 방식
+def solution(food):
+    answer = ''
+    for i,n in enumerate(food[1:]):
+        answer += str(i+1) * (n//2)
+    return answer + "0" + answer[::-1]
