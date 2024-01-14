819df339625ccddef9c2db6b4a29cbc74d5d8c49
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-14 21:11:57 +0900

    ref : 에라스토테네스의 체
    하지만 값이 커지면 느려지긴하다

--- 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.py
@@ -6,3 +6,17 @@ def solution(n):
             prime -= set(j for j in range(i*2, n+1, i))
             
     return len(prime)
+
+# 똑같은 에라스토테네스의 체 사용
+def solution(n):
+    num=set(range(2,n+1)) # 이런식으로 바로 생성 가능...
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
+
+에라토스테네스의 체
+0. 제곱근 이하의 배수들을 제거해나가 남는걸 세는 방식 -> 120까지면 11제곱인 121이 120보다 더 크므로 11미만의 배수들만 다 제거해주면 됨
+1. 처음 나오는 수는 나두고
+2. 처음 나오는 수의 배수는 싹다 제거
