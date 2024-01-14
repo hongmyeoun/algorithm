7c9da2ab70fb09e1c6faa9c388d2e8574eb6a302
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-14 20:01:58 +0900

    ref : 인덱싱으로 계산, 예술가

--- 프로그래머스/Lv.1/12901. 2016년/2016년.py
+++ 프로그래머스/Lv.1/12901. 2016년/2016년.py
@@ -30,3 +30,54 @@ def solution(a, b):
 # a - 1 == 4 -> 31 + 29 + 31 + 30 + 24
 # 145 - 1 == 144
 # 144 % 7 == 4 -> TUE
+
+# datetime사용
+import datetime
+
+def getDayName(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
+
+# 인덱싱으로 날짜 값 계산
+def getDayName(a,b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return day[(sum(month[:a-1])+b-1)%7]
+
+딕셔너리 만드는 것보다 이렇게 인덱싱을 하는게 더 편할 것 같다.
+
+# 번외 예술가
+def getDayName(a,b):
+    answer = ""
+    if a>=2:
+        b+=31
+        if a>=3:
+            b+=29#2월
+            if a>=4:
+                b+=31#3월
+                if a>=5:
+                    b+=30#4월
+                    if a>=6:
+                        b+=31#5월
+                        if a>=7:
+                            b+=30#6월
+                            if a>=8:
+                                b+=31#7월
+                                if a>=9:
+                                    b+=31#8월
+                                    if a>=10:
+                                        b+=30#9월
+                                        if a>=11:
+                                            b+=31#10월
+                                            if a==12:
+                                                b+=30#11월
+    b=b%7
+
+    if b==1:answer="FRI"
+    elif b==2:answer="SAT" 
+    elif b==3:answer="SUN"
+    elif b==4:answer="MON"
+    elif b==5:answer="TUE"
+    elif b==6:answer="WED"
+    else:answer="THU"
+    return answer
