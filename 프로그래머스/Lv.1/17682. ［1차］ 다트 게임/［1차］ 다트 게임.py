86de467d7dc948d2a1b4ddc4a0e34f57e3ae9d2d
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-15 01:55:52 +0900

    ref : re사용법

--- 프로그래머스/Lv.1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/Lv.1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -73,3 +73,83 @@ def split_input(input_str):
 # 그 값이 숫자가 아니면
 # 현재 값인 숫자를 score에 append하고 초기화
 # score에 마지막 요소에 현재 순회중인 값을 넣어줌
+
+# 정규식을 이용한 풀이
+import re
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0: # 첫번째에서 스타가 터졌을때 고려
+            dart[i-1] *= 2 # 이전 값 두배
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]] # 기존 리스트를 사용
+
+    answer = sum(dart)
+    return answer
+
+p = re.compile('(\d+)([SDT])([*#]?)')
+문자를 세 부위로 나눔
+(\d+) : \d는 숫자를 나타냄, +는 하나이상의 반복을 뜻함 -> 하나이상의 숫자가 반복되는 부분을 매칭해서 뽑아냄(끝날때)
+([SDT]) : [SDT]는 'S', 'D', 'T'중 하나랑 매칭해서 뽑아냄
+([*#]?) : '*', '#'중 하나가 있을수도 있고 없을 수도 있다. 있으면 그걸 뽑아냄, 없으면 빈값 ''을 줌
+
+dart = p.findall(dartResult)
+그렇게 뽑아낸 값들을 tuple묶고 list로 저장
+
+ex) dartResult = "1S2D*3T" 
+[('1', 'S', ''), ('2', 'D', '*'), ('3', 'T', '')]
+
+# 문자열을 전체를 순회하면서 풀이
+def solution(dartResult):
+    point = []
+    answer = []
+    dartResult = dartResult.replace('10','k') # 10은 1,0으로 순회가 되기때문
+    point = ['10' if i == 'k' else i for i in dartResult]
+    print(point)
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
+
+# 10을 안바꾸고 푼 풀이
+def solution(dartResult):
+    # 보너스 지수
+    dart = {'S':1, 'D':2, 'T':3}
+    # 점수를 저장할 리스트
+    scores = []
+    # 현재 순회 중인 문자의 인덱스
+    n = 0
+
+    for i, d in enumerate(dartResult):
+        # 현재 문자가 'S', 'D', 'T' 중 하나인 경우
+        if d in dart:
+            # 해당 보너스 지수를 적용하여 점수를 리스트에 추가
+            scores.append(int(dartResult[n:i])**dart[d])
+        # 현재 문자가 '*'인 경우
+        if d == "*":
+            # 바로 전과 현재 점수를 2배로 만듦
+            scores[-2:] = [x*2 for x in scores[-2:]]
+        # 현재 문자가 '#'인 경우
+        if d == "#":
+            # 현재 점수를 -1배로 만듦
+            scores[-1] = (-1)*scores[-1]
+        # 숫자가 아닌 문자를 만난 경우, 다음 숫자의 시작 인덱스를 기록
+        if not (d.isnumeric()):
+            n = i+1
+
+    # 최종 점수 계산
+    return sum(scores)
