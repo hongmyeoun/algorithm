03f2cad9a7a50a943969171b0999fe09d40f7784
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-14 21:03:37 +0900

    ref : 소수판별

--- 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.py
@@ -7,3 +7,15 @@ def solution(nums):
             answer += 1
     
     return answer
+
+# 에라스토테네스의 체를 이용한 소수 처리
+from itertools import combinations
+def prime_number(x):
+    answer = 0
+    for i in range(1,int(x**0.5)+1): # 제곱근 이하까지의 배수를 삭제하는 방식
+        if x%i==0:
+            answer+=1
+    return 1 if answer==1 else 0
+
+def solution(nums):
+    return sum([prime_number(sum(c)) for c in combinations(nums,3)])
