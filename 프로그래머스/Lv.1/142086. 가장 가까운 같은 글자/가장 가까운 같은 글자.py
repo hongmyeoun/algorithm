9e1c969bcf57eea04edfde18ca361573ce0d562e
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-15 16:44:18 +0900

    ref : dict사용 이유

--- 프로그래머스/Lv.1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/Lv.1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -19,3 +19,29 @@ def solution(s):
 # check에 중복요소가 있다면
 # check에 뒤에서부터 중복요소의 index를 가져와 현재 index에서 뺀값을 답에 추가
 # check에 현재값을 추가
+
+# dict이용
+def solution(s):
+    answer = []
+    dic = dict()
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[s[i]])
+        dic[s[i]] = i
+
+    return answer
+
+내 풀이의 check를 dic으로 만들어 풀이
+
+두 풀이의 시간복잡도
+
+첫 번째 해결책에서는 리스트 check을 사용하여 지금까지 만난 문자를 추적합니다. 
+각 문자의 마지막 발생 위치는 [::-1].index(s[c])를 사용하여 찾으며, 거리는 그에 따라 계산됩니다.
+이 해결책은 각 문자마다 check 리스트에서 마지막 발생을 검색하므로 시간 복잡도가 O(n^2)입니다. 
+왜냐하면 각 문자에 대해 O(n) 시간이 걸리기 때문입니다.
+
+반면에 두 번째 해결책은 각 문자의 최근 인덱스를 문자열에서 저장하기 위해 사전 dic을 사용합니다. 
+사전을 사용하는 주된 이점은 각 문자의 마지막 인덱스에 상수 시간(O(1))으로 액세스할 수 있다는 것입니다. 
+따라서 두 번째 해결책의 전체 시간 복잡도는 O(n)이 되며, 이는 첫 번째 해결책보다 효율적입니다.    
