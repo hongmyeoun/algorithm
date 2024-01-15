d42786f4d8f5d1d72573b8205738feaa7ca0dd81
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-15 18:41:31 +0900

    ref : string 라이브러리

--- 프로그래머스/Lv.1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/Lv.1/155652. 둘만의 암호/둘만의 암호.py
@@ -19,3 +19,26 @@ def solution(s, skip, index):
 # s의 문자와 skip의 문자는 겹칠 수 없다 -> 집합으로 풀이
 # 알파벳에서 skip을 전부뺌, 이후 두배 -> 유사 deque
 # 이러면 s의 문자를 알파벳 리스트에 대입해서 index를 더해주기만 하면 끝
+
+# ascii_lowercase 활용
+from string import ascii_lowercase
+
+def solution(s, skip, index):
+    result = ''
+
+    a_to_z = set(ascii_lowercase)
+    a_to_z -= set(skip)
+    a_to_z = sorted(a_to_z)
+    l = len(a_to_z)
+
+    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
+    for i in s:
+        result += a_to_z[(dic_alpha[i] + index) % l] # index%len(list) 이방법을 씀
+
+    return result
+
+# 라이브러리 안씀
+def solution(s, skip, index):
+    alphas = [chr(a) for a in range(ord("a"), ord("z")+1) if chr(a) not in skip]
+    return "".join([alphas[(alphas.index(a) + index) % len(alphas)] for a in s]) # index%len(list) 이방법을 씀
