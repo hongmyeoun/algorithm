1a21d3ed66eeb61a03930321b6ee3087d3227011
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-18 21:33:52 +0900

    [level 2] Title: 숫자의 표현, Time: 3.58 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,39 @@
+def solution(n):
+    result = 0
+    i = 1
+    while i <= n:
+        total = 0
+        j = i
+        while total < n:
+            total += j
+            j += 1
+        if total == n:
+            result += 1
+        i += 1
+    return result
+
+
+# def solution(n):
+#     result = 0
+#     i = 1
+#     while i<=n:
+#         if (n - i)%i == 0:
+#             result += 1
+#         i += 1
+#     return result
+
+# 15 - 1 = 14
+# 14//1 = 14, 14%1 = 0
+# 1+14
+# 15 - (1 + 2) = 12
+# 12//2 = 6, 12%2 = 0
+# 6+1 + 6+2 = 15
+# 15 - (1+2+3) = 9
+# 9//3 = 3, 9%3 = 0
+# 3+1 + 3+2 + 3+3 = 15
+# 15 - (1+2+3+4) = 5
+# 5%4 = 1
+# 15 - (1+2+3+4+5) = 0
+# 0%5 = 0, 0//5 = 0
+
+
