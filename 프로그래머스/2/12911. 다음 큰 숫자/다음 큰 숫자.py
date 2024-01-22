c3a4035f826e357c846b11c758361539ea8ecb5a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-22 22:27:03 +0900

    ref : 순서

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -7,3 +7,12 @@ def solution(n):
         else:
             n += 1
     return n
+
+# 순서를 바꾸면 반박문 위에 값을 안넣어도 된다
+def solution(n):
+    one = bin(n).count("1")
+    while True:
+        n += 1
+        if one == bin(n).count("1"):
+            break
+    return n
