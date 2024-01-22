de9c83051629d4f0f26d377f262b841ff7682965
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-01-22 22:53:42 +0900

    ref : 하나의 함수에서 또는 나눠서

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -12,3 +12,13 @@ def x_bin(x):
     zero = x.count("0")
     x = bin(x.count("1"))[2:]
     return x, zero
+
+
+# 하나의 함수에서
+def solution(s):
+    iteration, count = 0, 0
+    while s != '1':
+        iteration += 1
+        count += s.count('0')
+        s = bin(s.count('1'))[2:]
+    return [iteration, count]
