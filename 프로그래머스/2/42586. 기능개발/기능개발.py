dc90d41d81706d3c7b4d33a4bedd4e40e4144f13
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-02-08 01:03:09 +0900

    ref : pop()을 이용한 풀이

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -30,3 +30,37 @@ def solution(progresses, speeds):
 # 다음 요소가 그것 보다 작거나 같으면 temp(초기값 1) 에 1을 더함
 # 그렇지 않으면 그 값을 가장 큰값으로 저장
 # 그리고 temp값을 answer에 append
+
+# pop()을 이용한 풀이
+def solution(progresses, speeds):
+    print(progresses)
+    print(speeds)
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+# progress를 제거해나가면서 값을 추가하는 방식
+
+# progresses가 남아있을동안 반복
+# 만약 진행도가 100이 안됐다면(현재 진행도 + 시간에 따른 진행도) >= 0
+# 시간을 하루 추가
+# 배포카운터가 0보다 클경우(배포된 것이 있다면)
+# 배포카운터를 answer에 추가
+# 100이상이면
+# 가장 앞 작업, speed를 제거(배포)
+# 배포 카운트 +1
+
+# progresses가 모두 제거되었다면
+# 남은 배포카운터를 answer에 추가
