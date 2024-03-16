07534e386cb47f2538b9715a13349eacdee9d34a
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-03-16 23:31:44 +0900

    feat : 이분탐색을 이용한 풀이와 둘레를 이용한 풀이

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -106,3 +106,50 @@ def solution(brown, yellow):
 # b 2, y 5
 # b 7
 # [7, 4]
+
+이분 탐색을 이용한 풀이
+# x * y = brown + yellow
+# x + y = (brown + 4)//2
+
+# x + y 에서 x, y를 정한다. 이때 x와 y의 차가 작을 수록 그 곱은 더 커진다.
+# 이를 이용해서 이분 탐색으로 그 차의 크기를 조절해서 답을 구한다.
+
+# x <= y 라고 하면, x는 1 ~ (x+y)//2 의 값을 가진다.
+def solution(brown, yellow):
+    m = brown + yellow
+    s = (brown + 4) // 2
+
+    # x가 가질 수 있는 최대값
+    diff = (s // 2)
+
+    # x가 가질 수 있는 최대값의 절반
+    x = diff // 2
+    y = s - x
+    diff //= 2
+
+    while x * y != m:
+        if diff != 1:
+            diff //= 2
+
+        if x * y > m:
+            x -= diff
+            y += diff
+
+        else:
+            x += diff
+            y -= diff
+
+    return [y, x]
+
+출처 : https://school.programmers.co.kr/questions/52491
+고정값 x + y를 이용해 x*y의 순서쌍을 찾는 방식
+이렇게 풀게 되면 brown을 이용하게 되므로(s) 시간복잡도가 O(log brown)이 된다.
+기존 yellow를 이용했을땐 O(sqrt(yellow))였던 거보다 더 효율적이게 된다.
+
+# 둘레의 길이를 이용
+가로 * 세로 = 격자 합, 둘레의 합(가로*2 + 세로*2 - 겹치는부분4) = 갈색 둘레
+def solution(brown, red):
+    for i in range(1, int(red**(1/2))+1):
+        if red % i == 0:
+            if 2*(i + red//i) == brown-4:
+                return [red//i+2, i+2]
