9ba1e468f04167f561a0bd907e55bb7fbbf2ccd4
홍진 <139526068+hongmyeoun@users.noreply.github.com>
2024-03-21 20:45:22 +0900

    ref : Update 멀리 뛰기.kt

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -8,17 +8,38 @@ class Solution {
             return 2%1234567
         }
         
-        var pre = 1L
+        var pre = 1L // 파이썬과 다르게 타입 명시를 해야한다...
         var next = 2L
         var temp = 0L
         
         for (i in 1..n-2) {
             temp = pre
             pre = next
-            next = (temp + next)%1234567
+            next = (temp + next)%1234567 // next에서 값을 계산 안해서 한참 헤맸다...
         }
         
         answer = next
         return answer
+    }
+}
+
+// when을 사용한 결과값 반환...
+class Solution {
+    fun solution(n: Int): Long {
+        var first: Long = 1
+        var second: Long = 2
+    return when(n) {
+        1 -> first
+        2 -> second
+        else -> {
+            var current: Long = first + second
+            for (num in 3..n) {
+                current = (first + second) % 1234567
+                first = second
+                second = current
+            }
+            current
+        }
+    }
     }
 }
